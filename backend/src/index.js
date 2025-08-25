05ddf984dd74fbb2cc6abdcd76896b3aa08c841e
IshuAgrawal16 <izzu16012002@gmail.com>
2025-08-25 17:27:27 +0530

    Fix: Corrected Express.js wildcard route for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,11 +12,9 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server} from "./lib/socket.js";
 
 dotenv.config();
-const __dirname = path.resolve();
-
-
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -33,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
